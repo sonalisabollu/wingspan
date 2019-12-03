95f7424ae28f7db772e104e61e477974c6e6ce9f
Max Hernandez <max@navibanxmedia.com>
2019-12-02 16:34:54 -0800

    Updated DieRoller class

--- wingspan/src/DieRoller.java
+++ wingspan/src/DieRoller.java
@@ -3,27 +3,73 @@ import java.util.ArrayList;
 public class DieRoller
 {
     // instance variables - replace the example below with your own
-    ArrayList<Die> dice;
-    private int facesPerDie = 6;
+    ArrayList<Die> diceIn;
+    ArrayList<Die> diceOut;
+    private int facesPerDie;
+    private int dieCount;
+    
     
     /**
      * Constructor for objects of class BirdFeeder
      */
-    public DieRoller(int die_count, int facesPerDie)
+    public DieRoller(int dieCount, int facesPerDie)
     {
-        dice = new ArrayList<>();
-        
-         for(int i = 0; i<=die_count; i++){
+        diceIn = new ArrayList<>();
+        diceOut = new ArrayList<>();
+        this.facesPerDie = facesPerDie;
+         for(int i = 0; i<dieCount; i++){
              Die die = new Die(facesPerDie);
-             this.dice.add(die);
+             this.diceIn.add(die);
         }
     }
     
+    /**
+     * Roll each die
+     */
     public void rollAll(){
-        this.dice.forEach((die) -> die.roll()); 
+        this.diceIn.forEach((die) -> die.roll()); 
     }
     
-    public void removeDie(int value){
-        //remove single die from Die Roller
+    
+    /**
+     * Return the curren value for each die
+     * @return Array of values for each die
+     */
+    public int [] getValues(){
+        int[] values = new int[facesPerDie];
+        for(int i = 0; i< diceIn.size(); i++){
+            values[i] = diceIn.get(i).getCurrentFace();
+        }
+        return values;
+    }
+    
+    /**
+     * Remove a single die from play
+     * @param index index of die to be removed
+     */
+     public void removeDie(int index){
+         diceOut.add(diceIn.get(index));
+         diceIn.remove(index);
+    }
+    
+    /**
+     * Get number of dice on play
+     */
+    public int getDieCount(){
+        return diceIn.size();
+    }
+    
+    /**
+     * Restore all dice and randomize them
+     */
+    public void reset(){
+        if(this.diceOut.size() <1){
+            return;
+        }
+        for(int i = 0; i<= diceOut.size(); i++){
+            this.diceIn.add(diceOut.get(i));
+        }
+        
+        this.diceIn.forEach((die) -> die.roll()); 
     }
 }
